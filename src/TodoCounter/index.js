4ce0a1f29b1074a52a2d4720e8def70702600e31
Yaret BP <helliox2014@gmail.com>
2023-01-11 21:34:58 -0600

    fix: general details
    -Package json
    -Label

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -6,7 +6,7 @@ function TodoCounter({total, completed}) {
     const {totalTodos, completedTodos} = React.useContext(TodoContext);
 
     return(
-        <h2 className='TodoCounter'>Had completed {completedTodos} of {totalTodos} TODOs</h2>
+        <h2 className='TodoCounter'>Have completed {completedTodos} of {totalTodos} TODOs</h2>
     );
 }
 
